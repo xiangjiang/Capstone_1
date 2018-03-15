9cda7c7dfbcf970a51bd117286db4df3e0213de4
student <jx.for.jiangxiang@gmail.com>
2018-03-15 10:46:02 -0700

    update waypoint update and dbw_node

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,21 @@
+from yaw_controller import YawController
+from pid import PID
+from lowpass import LowPassFilter
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, vehicle_mass,fuel_capacity,brake_deadband,decel_limit,accel_limit,wheel_radius,wheel_base,steer_ratio,max_lat_accel,max_steer_angle, min_speed):
         # TODO: Implement
-        pass
+        self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
+        kp = 0.12
+        ki = 0.006
+        kd = 3.0
+        self.pid = PID(kp, ki, kd)
 
-    def control(self, *args, **kwargs):
+    def control(self):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
         return 1., 0., 0.
