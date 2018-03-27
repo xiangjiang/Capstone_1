3f802bae0746c85b470c49b3e67a7049c1580b77
xiangjiang <jx.for.jiangxiang@gmail.com>
2018-03-27 16:50:06 -0700

    transfer some files

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,19 +1,24 @@
 from styx_msgs.msg import TrafficLight
+from simulator import SimuatorModel
+from carla import CarlaModel
+import os
 
 class TLClassifier(object):
-    def __init__(self):
-        #TODO load classifier
-        pass
+    def __init__(self, scenario):
+        if scenario == "simulator":
+            self.model = SimuatorModel()
+        else:
+            curr_dir = os.path.dirname(os.path.realpath(__file__))
+            self.model = CarlaModel(curr_dir+"/models/frozen_inference_graph.pb")
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
 
         Args:
-            image (cv::Mat): image containing the traffic light
+            image (cv::Mat): image containing the traffic light, BGR channel
 
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        return self.model.predict(image)
